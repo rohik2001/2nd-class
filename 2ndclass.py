e73326e0cffb109c7263a0248f8fad79f63b2d08
rohik2001 <mdhabibshayim2001@gmail.com>
2024-11-11 11:39:01 +0600

    Add files via upload

---
+++ 2ndclass.py
@@ -0,0 +1,40 @@
+#customerName="Rohik"
+'''customerName=input("Enter Customer Name= ")
+customerId=int(input("Enter Customer ID= "))
+productQuantity=int(input("Enter quatitiy= "))
+price=float(input("Enter amount= "))
+print(customerName)
+print("Customer Name is: ", customerName)
+print("Customer ID, product price: ", customerId, price)
+print(f"Customer Name is {customerName}. Customer Id is {customerId}, Product Quantity {productQuantity} and The proice is {price}")
+'''
+#basic student info
+Student_Name = input('Enter student Name')
+Student_Age = input('Enter Student Age')
+Student_Parent_Name = input('Enter Student parent Name')
+Student_Grade = input('Enter Student Grade')
+Student_number = int(input('Enter Student Mobile Number'))
+#Marks
+Type_of_Exam = input('Enter student Anual Exam or Test Exam Type')
+Sub_English = int(input('Enter your Eng mark in exam'))
+Sub_Math = int(input('Enter your Math mark in exam'))
+Sub_Ict = int(input('Enter your Ict mark in exam'))
+# avarage mark
+Total_mark= Sub_English + Sub_Math + Sub_Ict
+Avarage_mark= Total_mark/3
+#display all
+print(f"Student Name is : {Student_Name}")
+print(f"Student Age is : {Student_Age}")
+print(f"Student Parent Name is : {Student_Parent_Name}")
+print(f"Student Grade is : {Student_Grade}")
+print(f"Student Number is : {Student_number}")
+print(f"Student Type of Exam : {Type_of_Exam}")
+print(f"Student English Subject mark is : {Sub_English}")
+print(f"Student Math Subject mark is : {Sub_Math}")
+print(f"Student Ict Subject mark is : {Sub_Ict}")
+print(f"Student Total Mark is : {Total_mark}")
+print(f"Student Avarage Mark is : {Avarage_mark}")
+
+
+
+
